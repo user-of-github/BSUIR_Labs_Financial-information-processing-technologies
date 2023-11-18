6c53735771aef415dd4cfa3b1bc87408a4b62958
Nikita <nikitosminsk100@gmail.com>
2023-11-18 20:57:17 +0300

    feat: animated scaling on click

--- LR3-4/banking/app/constants.tsx
+++ LR3-4/banking/app/constants.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image';
 import { Route } from '@/core/types';
 
 export const HeadingElement = (
-  <Link href="/home" className="flex items-center">
+  <Link href="/home" className="flex items-center transition active:scale-95">
     <Image src="/assets/images/logo.png" height={25} width={25} objectFit="contain" alt="Banking logo" />
     <span className="ml-3 self-center whitespace-nowrap text-2xl font-semibold dark:text-white">Online Banking</span>
   </Link>
--- LR3-4/banking/components/UI/Button.tsx
+++ LR3-4/banking/components/UI/Button.tsx
@@ -10,12 +10,13 @@ interface ButtonProps {
 
 export const Button = ({ type, text, onClick, appearance, className }: ButtonProps): JSX.Element => {
   return (
-    <button type={type} onClick={onClick} className={clsx(buttonStyles[appearance], className)}>
+    <button type={type} onClick={onClick} className={clsx(buttonStyles[appearance], animated, className)}>
       {text}
     </button>
   );
 };
 
+const animated = 'transition active:scale-90' as const;
 const buttonStyles = {
   primary:
     'text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800',
