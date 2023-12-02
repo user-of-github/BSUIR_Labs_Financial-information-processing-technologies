79b8706b81b5380a0a342fdb67879a44e0680018
Nikita <nikitosminsk100@gmail.com>
2023-12-02 20:28:27 +0300

    feat: validation test

--- LR3-4/banking/core/validators.ts
+++ LR3-4/banking/core/validators.ts
@@ -12,7 +12,7 @@ type ValidationResult =
 
 type ValidatorFunction<ValueType> = (value: ValueType) => ValidationResult;
 
-type ValidationPair<ValueType> = [ValueType, ValidatorFunction<ValueType>, string];
+export type ValidationPair<ValueType> = [ValueType, ValidatorFunction<ValueType>, string];
 
 export const inputFieldValidator: ValidatorFunction<string> = (value: string) => {
   const NAME_FIELD_MIN_LENGTH = 3;
@@ -28,13 +28,25 @@ export const inputFieldValidator: ValidatorFunction<string> = (value: string) =>
   }
 };
 
-export const passportIdValidator: ValidatorFunction<string> = (value: string) => {
-  const PASSPORT_ID_LENGTH = 9;
-  if (value.trim().length !== PASSPORT_ID_LENGTH) {
+const PASSPORT_ID_LENGTH = 9;
+const PASSPORT_SERIES = new Set(['AB', 'BM', 'HB', 'KH', 'MP', 'MC', 'KB', 'PP', 'SP', 'DP']);
+const DIGITS = '0123456789';
+export const passportIdValidator: ValidatorFunction<string> = (val: string) => {
+  const value = val.trim();
+  if (value.length !== PASSPORT_ID_LENGTH) {
     return { status: false, error: 'Passport ID must contain exactly 9 values, ex.: MP1234567' };
-  } else {
-    return { status: true };
   }
+  const series = value.slice(0, 2).toUpperCase();
+  if (!PASSPORT_SERIES.has(series)) {
+    return { status: false, error: 'Passport ID must start with series, ex.: MP, KH, MC, KB ...' };
+  }
+
+  const number = value.slice(2);
+  if (![...number].every(num => DIGITS.includes(num) )) {
+    return { status: false, error: 'Passport ID must have a number after series, ex.: MP1234567' };
+  }
+
+  return { status: true };
 };
 
 export const emailValidator: ValidatorFunction<string> = (value: string) => {
---
+++ LR3-4/banking/tests/validators.test.ts
@@ -0,0 +1,51 @@
+import { emailValidator, passportIdValidator, phoneValidator, validate, ValidationPair } from '../core/validators';
+
+describe('Validation tests', () => {
+ describe('Base validators', () => {
+   test('Passport id validator', () => {
+     expect(passportIdValidator('MP12345').status).toBeFalsy();
+     expect(passportIdValidator('MP').status).toBeFalsy();
+     expect(passportIdValidator('123456').status).toBeFalsy();
+     expect(passportIdValidator('MP0000000').status).toBeTruthy();
+     expect(passportIdValidator('MR0000000').status).toBeFalsy();
+     expect(passportIdValidator('AR0000000').status).toBeFalsy();
+     expect(passportIdValidator('KB0000000').status).toBeTruthy();
+   });
+
+   test('Phone validator', () => {
+     expect(phoneValidator('+375').status).toBeFalsy();
+     expect(phoneValidator('+015589').status).toBeFalsy();
+     expect(phoneValidator('+375290000000').status).toBeTruthy();
+     expect(phoneValidator('+375000000000').status).toBeFalsy();
+   });
+
+   test('Email validator', () => {
+     expect(emailValidator('+375').status).toBeFalsy();
+     expect(emailValidator('email-example@').status).toBeFalsy();
+     expect(emailValidator('email@@').status).toBeFalsy();
+     expect(emailValidator('real-email@com.com').status).toBeTruthy();
+   });
+ });
+
+ describe('Sequence validator', () => {
+   test('Works properly', () => {
+     const validData = [
+       ['email@emailcom', emailValidator, ''] as ValidationPair<string>,
+       ['+375331231231', phoneValidator, ''] as ValidationPair<string>,
+     ];
+
+     let validationResult = validate(validData);
+
+     expect(validationResult.status).toBeTruthy();
+
+     const invalidData = [
+       ['email@', emailValidator, ''] as ValidationPair<string>,
+       ['+375331231231', phoneValidator, ''] as ValidationPair<string>,
+     ];
+
+     validationResult = validate(invalidData);
+
+     expect(validationResult.status).toBeFalsy();
+   });
+ });
+});
