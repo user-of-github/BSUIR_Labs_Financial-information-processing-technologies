e03ae3f05775659d592e484d5b1d954922630f4e
Nikita <nikitosminsk100@gmail.com>
2023-12-02 12:16:20 +0300

    feat: set up tests for bank account

--- LR3-4/banking/tests/database.test.ts
+++ LR3-4/banking/tests/database.test.ts
@@ -1,43 +1,91 @@
 import { SupabaseClient, createClient } from '@supabase/supabase-js';
+import { getAccountData, user1, user2 } from './mocks';
 
-import exp from 'constants';
-
-import { email, password, phone, signUpMetadata } from './mocks';
 
 describe('Supabase logic', () => {
   let Supabase: SupabaseClient;
-  let createdUserId: string;
+  let SupabaseAdmin: SupabaseClient;
 
-  /*  Initialize SupaBase Client, Register a new user, Save its id  */
+  let createdUser1Id: string;
+  let createdUser2Id: string;
+
+  /*  Initialize Supabase Client, Register 2 new users, Save their id  */
   beforeAll(async () => {
+    SupabaseAdmin = createClient(process.env.DB_URL as string, process.env.DB_KEY_ADMIN as string);
     Supabase = createClient(process.env.DB_URL as string, process.env.DB_KEY as string);
-    const signInResponse = await Supabase.auth.signUp({ email, password, phone, options: { data: signUpMetadata } });
 
-    if (signInResponse.data.user) {
-      createdUserId = signInResponse.data.user.id;
-      console.log(createdUserId);
+    const signUpResponse1 = await Supabase.auth.signUp(user1);
+    const signUpResponse2 = await Supabase.auth.signUp(user2);
+
+    if (signUpResponse1.data.user) {
+      createdUser1Id = signUpResponse1.data.user.id;
+      console.log('Signed up a new user: ', createdUser1Id);
+    }
+
+    if (signUpResponse2.data.user) {
+      createdUser2Id = signUpResponse2.data.user.id;
+      console.log('Signed up a new user: ', createdUser2Id);
     }
   });
 
-  describe('Sign up logic works correctly', () => {
-    test('Log in after sign up is successful', async () => {
-      const logInResult = await Supabase.auth.signInWithPassword({ email, password });
+  describe('Sign up logic worked correctly', () => {
+    test.each([user1, user2])('Logs in successfully, checks trigger for PROFILES table worked',async user => {
+      const logInResult = await Supabase.auth.signInWithPassword({
+        email: user.email,
+        password: user.password
+      });
       expect(logInResult.error).toBe(null);
-    });
-
-    test('Trigger for PROFILES table worked correctly', async () => {
+      const userId = logInResult.data.user?.id;
       const profilesResult = await Supabase.from('Profiles').select('*');
+      expect(profilesResult.data?.length).toEqual(1); // RLS: user has permissions only to his account
 
-      expect(profilesResult.data).not.toBe(null);
-      const profile = profilesResult.data?.find((item) => item.id === createdUserId);
+      const profile = profilesResult.data?.find((item) => item.id === userId);
       expect(profile).not.toBe(null);
-      expect(profile).toEqual({ ...signUpMetadata, id: createdUserId });
+      expect(profile).toEqual({ ...user.options.data, id: userId });
+
+      const logOutResult = await Supabase.auth.signOut();
+      expect(logOutResult.error).toBe(null);
     });
   });
 
-  /* Remove a created user */
-  afterAll(async () => {
-    const deleteResult = await Supabase.auth.admin.deleteUser(createdUserId);
-    console.log(deleteResult);
+  describe('Payments logic works correctly', () => {
+    test('Unauthorized user can not create a new money account', async () => {
+      const unauthorizedResult = await Supabase.from('BankAccounts').insert(getAccountData('84884', 'Not valid access'));
+      expect(unauthorizedResult.data).toBe(null);
+      expect(unauthorizedResult.error).not.toBe(null);
+    });
+
+    test.each([user1, user2])('Logs in successfully and creates virtual money account (card)', async user => {
+      const logInResult = await Supabase.auth.signInWithPassword({
+        email: user.email,
+        password: user.password
+      });
+      expect(logInResult.error).toBe(null);
+
+      // Currently there are no opened money accounts
+      let moneyAccounts = await Supabase.from('BankAccounts').select('*');
+      expect(moneyAccounts.error).toBe(null);
+      expect(moneyAccounts.data).toHaveLength(0);
+
+      const accountData = getAccountData(logInResult.data.user!.id, `Account for ${user1.options.data.first_name}`);
+      const createMoneyAccountResult = await Supabase.from('BankAccounts').insert(accountData);
+      expect(createMoneyAccountResult.error).toBe(null);
+
+      moneyAccounts = await Supabase.from('BankAccounts').select('*');
+      expect(moneyAccounts.error).toBe(null);
+      expect(moneyAccounts.data).toHaveLength(1);
+
+      const logOutResult = await Supabase.auth.signOut();
+      expect(logOutResult.error).toBe(null);
+    });
+  });
+
+  /* Remove created users */
+ afterAll(async () => {
+    const deleteResult1 = await SupabaseAdmin.auth.admin.deleteUser(createdUser1Id);
+    expect(deleteResult1.error).toBe(null);
+
+    const deleteResult2 = await SupabaseAdmin.auth.admin.deleteUser(createdUser2Id);
+    expect(deleteResult2.error).toBe(null);
   });
 });
--- LR3-4/banking/tests/mocks.ts
+++ LR3-4/banking/tests/mocks.ts
@@ -1,15 +1,44 @@
 import { UserMetaData } from '@/core/types';
 
-export const email: Readonly<string> = 'testemail@test.com';
-export const password: Readonly<string> = 'passworD@123456789';
-export const phone: Readonly<string> = '+375290000000';
 
 type SignUpMetadata = Readonly<Omit<UserMetaData, 'id'>>;
-
-export const signUpMetadata: Readonly<SignUpMetadata> = {
+export const signUpMetadata1: Readonly<SignUpMetadata> = {
   first_name: 'Anthony',
   middle_name: 'Howard',
   last_name: 'Stark',
   passport_id: 'MP1234567',
   client_from_date: new Date().toDateString()
 };
+
+export const user1 = {
+  email: 'anthony@test.com',
+  password: 'anthonyPassword2023TOFI',
+  phone: '+375290000000',
+  options: { data: signUpMetadata1 }
+} as const;
+
+export const signUpMetadata2: Readonly<SignUpMetadata> = {
+  first_name: 'Peter',
+  middle_name: 'Benjamin',
+  last_name: 'Parker',
+  passport_id: 'MP7654321',
+  client_from_date: new Date().toDateString()
+};
+
+export const user2 = {
+  email: 'peterparker@test.com',
+  password: 'peterPassword2023TOFI',
+  phone: '+375330000000',
+  options: { data: signUpMetadata2 }
+} as const;
+
+export const getAccountData = (owner: string, name: string) => ({
+    amount: 1000,
+    currency: 1,
+    date_opened: new Date().toDateString(),
+    name,
+    owner
+} as const);
+
+export const invalidTransferAmounts = [-1000, 500000] as const;
+export const validTransferAmount = 500;
