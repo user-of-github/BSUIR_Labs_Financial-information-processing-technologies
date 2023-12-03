6d1196ed5d4b5003e0f3be752fb15ae00b8e4872
Nikita <nikitosminsk100@gmail.com>
2023-12-03 16:16:47 +0300

    cleanup

--- LR3-4/banking/components/MoneyAccountRowCard.tsx
+++ LR3-4/banking/components/MoneyAccountRowCard.tsx
@@ -6,7 +6,7 @@ type MoneyAccountRowCardProps = Omit<MoneyAccount, 'owner'>;
 
 export const MoneyAccountRowCard = (props: MoneyAccountRowCardProps) => {
   return (
-    <div className="w-full p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+    <section className="w-full p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
       <h5 className="mb-4 text-xl font-semibold tracking-tight text-gray-900 dark:text-white truncate">
         Money account <br /> {props.name && props.name}
         <br />
@@ -33,6 +33,6 @@ export const MoneyAccountRowCard = (props: MoneyAccountRowCardProps) => {
           />
         </svg>
       </Link>
-    </div>
+    </section>
   );
 };
